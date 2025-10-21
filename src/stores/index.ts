159e48692213278e4ee09b1a70e1207bf6f50fd2
Dave Porras <david.porras@personalexterno.femsasalud.com>
2025-10-21 15:40:52 -0300

    tailwind config

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,13 +1,9 @@
 import type { BIP32Interface } from 'bip32';
-import type { Network } from 'bitcoinjs-lib';
 import { create } from 'zustand';
-import { bitcoin } from '../config';
 
 const useStore = create((set) => ({
 	masterKey: null,
-	network: bitcoin.networks.bitcoin,
 	setMasterKey: (masterKey: BIP32Interface) => set({ masterKey }),
-	setNetwork: (network: Network) => set({ network }),
 }));
 
 export default useStore;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import wasm from 'vite-plugin-wasm';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), wasm()],
+	plugins: [react(), wasm(), tailwindcss()],
 	server: {
 		hmr: { overlay: true },
 	},
