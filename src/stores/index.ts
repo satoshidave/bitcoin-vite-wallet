f84f91835d04723dd9d80e603b1031cec1315565
Dave Porras <david.porras@personalexterno.femsasalud.com>
2025-10-21 15:39:20 -0300

    tailwind config

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,9 +1,13 @@
 import type { BIP32Interface } from 'bip32';
+import type { Network } from 'bitcoinjs-lib';
 import { create } from 'zustand';
+import { bitcoin } from '../config';
 
 const useStore = create((set) => ({
 	masterKey: null,
+	network: bitcoin.networks.bitcoin,
 	setMasterKey: (masterKey: BIP32Interface) => set({ masterKey }),
+	setNetwork: (network: Network) => set({ network }),
 }));
 
 export default useStore;
